5bdcd016c773b4b71e1b7776c7f4ad2fdaa4b3ff
Carlos J Ramirez <carlosjramirez11@gmail.com>
2022-04-21 17:12:35 -0700

    Started setting up DB class

---
+++ config/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+class Database
+{
+//DB Params
+    private $host = "locahost";
+    private $db = "blog";
+    private $username = "root";
+    private $password = "123456";
+    private $conn;
+
+    //DB Connect
+    public function connect()
+    {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=$this->host; dbname=$this->db", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $error) {
+            echo "Connection Error: $error->getMessage();";
+        }
+    }
+}
