944135a2d9acccc2725e41de5660d85203bf2034
Carlos Ramirez <carlosjramirez11@gmail.com>
2022-04-25 16:47:30 -0700

    Added create method in Post

--- models/Post.php
+++ models/Post.php
@@ -88,4 +88,42 @@ class Post
 
     }
 
+    //Create Post
+    public function create()
+    {
+        //Create Query
+        $query = "INSERT INTO
+        $this->table
+            SET
+                title = :title,
+                body = :body,
+                author = :author,
+                category_id = :category_id";
+
+        //Prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        //Clean Data
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars(strip_tags($this->body));
+        $this->author = htmlspecialchars(strip_tags($this->author));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+        //Bind Data
+        $stmt->bindParam(":title", $this->title);
+        $stmt->bindParam(":body", $this->body);
+        $stmt->bindParam(":author", $this->author);
+        $stmt->bindParam(":category_id", $this->category_id);
+
+        //Execute Query
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            //Print Error
+            printf("Error: %s.\n", $stmt->error);
+            return false;
+        }
+
+    }
+
 }
