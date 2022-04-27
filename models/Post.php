57bf32a108cb15d107cb0d77343821766c633bdc
Carlos Ramirez <carlosjramirez11@gmail.com>
2022-04-26 19:26:28 -0700

    Added edit method to Post class

--- models/Post.php
+++ models/Post.php
@@ -126,4 +126,46 @@ class Post
 
     }
 
+    //Update Post
+    public function update()
+    {
+        //Create Query
+        $query = "UPDATE
+        $this->table
+            SET
+                title = :title,
+                body = :body,
+                author = :author,
+                category_id = :category_id
+            WHERE
+                id = :id";
+
+        //Prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        //Clean Data
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars(strip_tags($this->body));
+        $this->author = htmlspecialchars(strip_tags($this->author));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        //Bind Data
+        $stmt->bindParam(":title", $this->title);
+        $stmt->bindParam(":body", $this->body);
+        $stmt->bindParam(":author", $this->author);
+        $stmt->bindParam(":category_id", $this->category_id);
+        $stmt->bindParam(":id", $this->id);
+
+        //Execute Query
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            //Print Error
+            printf("Error: %s.\n", $stmt->error);
+            return false;
+        }
+
+    }
+
 }
