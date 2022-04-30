413beea985b6e9c8dbd2be46d69e0ea3e0a1fb64
Carlos J Ramirez <carlosjramirez11@gmail.com>
2022-04-29 19:20:58 -0700

    created update file for categories

---
+++ api/category/update.php
@@ -0,0 +1,36 @@
+<?php
+
+//Headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: PUT");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With");
+
+include_once "../../config/Database.php";
+include_once "../../models/Category.php";
+
+//Instantiate DB & Connect
+$database = new Database();
+$db = $database->connect();
+
+//Instantiate Category Object
+$category = new Category($db);
+
+//Get Raw Category Data
+$data = json_decode(file_get_contents("php:://input"));
+
+//Set ID to Update
+$category->id = $data->id;
+
+$category->name = $data->name;
+
+//Create Post
+if ($post->update()) {
+    echo json_encode(
+        array("message" => "Post Updated")
+    );
+} else {
+    echo json_encode(
+        array("message" => "Post Not Updated")
+    );
+}
